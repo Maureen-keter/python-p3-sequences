687e76452e836d4e851835763ceea996dabb4583
maureen <maureen.chelangat@student.moringaschool.com>
2023-11-24 10:38:46 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    if length ==0:
-        print([])
-    elif length==1:
-        print([0])
-    elif length==2:
-        print([0,1])              
-    else:
-        print ([0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
+    fib_sequence = []
+    
+    for i in range(length):
+        if i ==0:
+            fib_sequence.append(0)
+        elif i==1:
+            fib_sequence.append(1)
+        else:
+            fib_sequence.append(fib_sequence[-1]+fib_sequence[-2])
+    print(fib_sequence)
+print_fibonacci(3)
+    
     
     
